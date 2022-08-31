7e1f5dfcff806c6e90053b5a35c0ec9c82a433bf
siddharth <siddharth.sabron@gmail.com>
2022-08-31 23:20:41 +0530

    test

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ const PORT = 3300;
 
 //Creating our server
 app.get("/", (req, res) => {
-  const response = { success: true, message: "Give me a bottle of rum" };
+  const response = { success: true, message: "Give me a bottle of rum!" };
   res.status(200).json(response);
 });
 app.listen(PORT);
