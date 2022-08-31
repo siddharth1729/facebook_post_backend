e2863fbf54ec0e3a6403b87ac459a057049f7aff
siddharth <siddharth.sabron@gmail.com>
2022-08-31 23:18:17 +0530

    Project setup

---
+++ index.js
@@ -0,0 +1,20 @@
+/**************************************************
+ *             author : siddharth                  *
+ **************************************************/
+
+const express = require("express");
+const app = express();
+const dotenv = require("dotenv");
+dotenv.config();
+
+//DEFINIG PORT
+const PORT = 3300;
+
+//Creating our server
+app.get("/", (req, res) => {
+  const response = { success: true, message: "Give me a bottle of rum" };
+  res.status(200).json(response);
+});
+app.listen(PORT);
+
+console.info(`listning on ==>> http://localhost:${PORT}`);
